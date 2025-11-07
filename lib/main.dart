cfab9499cdc465ee2e7033e25d9e895010df2b86
Mohamed Assem <mohamedassem663@gmail.com>
2025-11-07 16:00:37 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TuneApp());
+}
+
+class TuneApp extends StatelessWidget {
+  const TuneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
